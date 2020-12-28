51b6e312e0707d292a1be6763ce6cfea1dc14d12
MillarayM <millaray.sw@gmail.com>
2020-12-28 12:43:27 -0300

    test 2

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      test.........<br>
+      test.........test  XD<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
